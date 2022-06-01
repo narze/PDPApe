5ad3a1e80b990d44ce5596c80fedca21f53a7b9a
Manassarn Manoonchai <manassarn@gmail.com>
2022-06-01 22:23:51 +0700

    feat: add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import Unocss from "unocss/vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Unocss({
+      /* options */
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
